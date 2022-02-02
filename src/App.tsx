f16330fc5a58dfa16ed1a40f24dfc4c211547c93
liuxilei <liuxileioo@qq.com>
2022-02-02 12:02:37 +0800

    feat: prettier测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return;
-  <div className="App">121</div>;
+  useEffect(() => {
+    console.log(1);
+  });
+  return <div>111</div>;
 }
 
 export default App;
